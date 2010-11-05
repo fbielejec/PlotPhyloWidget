5e3569cab918f8d7ef63ba048e92f57f7f307329
fbielejec <fbielejec@gmail.com>
2010-11-05 10:13:01 +0100

    starting script

--- PlotPhylo.r
+++ PlotPhylo.r
@@ -2,7 +2,7 @@
 #---RUN---#
 ###########
 #change the path for the directory containing all the sources:
-setwd("/home/filip/Dropbox/Phyleography/PlotPhylo")
+setwd("/home/Dropbox/Phyleography/PlotPhylo")
 
 source("libs.r")
 source("ggplot_utils.r")
