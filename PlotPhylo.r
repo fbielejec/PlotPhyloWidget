b29d31b6b2b6a0c0ca169ec66d86a89ea8db338c
fbielejec <fbielejec@gmail.com>
2010-11-05 10:12:02 +0100

    GUI gWidgets style

---
+++ PlotPhylo.r
@@ -0,0 +1,11 @@
+###########
+#---RUN---#
+###########
+#change the path for the directory containing all the sources:
+setwd("/home/filip/Dropbox/Phyleography/PlotPhylo")
+
+source("libs.r")
+source("ggplot_utils.r")
+source("gui.r")
+
+
