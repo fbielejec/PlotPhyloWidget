254fd290a068c43cd6c32fb3c6cc3af57a38c95f
fbielejec <fbielejec@gmail.com>
2010-12-21 12:50:39 +0100

    libraries

--- libs.r
+++ libs.r
@@ -1,10 +1,13 @@
 #####################
 #---LIBRARIES GTK---#
 #####################
-options(guiToolkit = "RGtk2")
 library(gWidgets)
-library(gWidgetsRGtk2)
 library(ape)
 library(ggplot2)
 library(cairoDevice)
+options(guiToolkit = "RGtk2")
+library(gWidgetsRGtk2)
+
+#options(guiToolkit = "tcltk") 
+#library(gWidgetstcltk) 
 
